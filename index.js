d1d09e0459d1275b3455d414f4aad472223e5d0e
Fahmi Aziz <fahmiaziz031102@gmail.com>
2024-04-12 02:17:29 +0700

    menambahkan cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
 require('dotenv').config()
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cors())
 
 
 const postsRouter = require('./routes/posts.router')
