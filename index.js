062782faecbb0e862f325cb4b48daa4f2f7e485f
Fahmi Aziz <fahmiaziz031102@gmail.com>
2024-04-12 05:52:46 +0700

    ubah router

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 const postsRouter = require('./routes/posts.router')
 const authRouter = require('./routes/auth.router')
 
-app.use("/api/v1/posts", postsRouter)
+app.use("/api/v1/wish", postsRouter)
 app.use("/api/v1/auth", authRouter)
 
 const PORT = process.env.PORT || 5000
