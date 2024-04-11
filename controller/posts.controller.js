c357b27204adbb4c57b6951dba85fdf3e1e596d4
Fahmi Aziz <fahmiaziz031102@gmail.com>
2024-04-12 05:31:12 +0700

    up table

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -2,7 +2,7 @@ const pool = require("../database/index")
 const postsController = {
     getAll: async (req, res) => {
         try {
-            const [rows, fields] = await pool.query("select * from posts")
+            const [rows, fields] = await pool.query("select * from wish")
             res.json({
                 data: rows
             })
@@ -16,7 +16,7 @@ const postsController = {
     getById: async (req, res) => {
         try {
             const { id } = req.params
-            const [rows, fields] = await pool.query("select * from posts where id = ?", [id])
+            const [rows, fields] = await pool.query("select * from wish where id = ?", [id])
             res.json({
                 data: rows
             })
@@ -30,7 +30,7 @@ const postsController = {
     create: async (req, res) => {
         try {
             const { id, name, presence, message, created_at } = req.body
-            const sql = "insert into posts (id, name, presence, message, created_at) values (?, ?)"
+            const sql = "insert into wish (id, name, presence, message, created_at) values (?, ?)"
             const [rows, fields] = await pool.query(sql, [id, name, presence, message, created_at])
             res.json({
                 data: rows
@@ -46,7 +46,7 @@ const postsController = {
         try {
             const { title, content } = req.body
             const { id } = req.params
-            const sql = "update posts set title = ?, content = ? where id = ?"
+            const sql = "update wish set title = ?, content = ? where id = ?"
             const [rows, fields] = await pool.query(sql, [title, content, id])
             res.json({
                 data: rows
@@ -61,7 +61,7 @@ const postsController = {
     delete: async (req, res) => {
         try {
             const { id } = req.params
-            const [rows, fields] = await pool.query("delete from posts where id = ?", [id])
+            const [rows, fields] = await pool.query("delete from wish where id = ?", [id])
             res.json({
                 data: rows
             })
